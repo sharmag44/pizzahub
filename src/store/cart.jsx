14977d04159b3eb1db8d73516be3ef5e01620a9b
shambhu_bcoder <shambhunath@bcoder.in>
2024-02-03 20:23:06 +0530

    code

--- src/store/cart.jsx
+++ src/store/cart.jsx
@@ -2,7 +2,9 @@ import { create } from 'zustand';
 
 export const addToCard = create((set) => ({
     items: [],
+    total: 0,
     addItem: (item) => set((state) => ({ items: [...state.items, item] })),
+
 }))
 
 
